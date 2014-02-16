e21a30bb27e4added1b47a994cd9157c9e78bddb
root <alevellop@gmail.com>
2014-02-16 13:41:41 +0100

    añadido user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -34,7 +35,7 @@ class UsersController < ApplicationController
   end
 
   def update
-    if @user.update_attributes(user_params)  
+    if @user.update_attributes(user_params)
       flash[:success]  ="Profile updated"
       redirect_to @user
     else
@@ -57,11 +58,6 @@ class UsersController < ApplicationController
                                    :password_confirmation)
     end
 
-    # Before filters    
-    def signed_in_user
-      store_location
-      redirect_to signin_url, notice: "Please sign in." unless signed_in?
-    end
 
     def correct_user
       @user = User.find(params[:id])
